cdfb6ab874737db49c1bac6b9b5faa59145e5e0a
Supergamerz 19 <robbjason28@gmail.com>
2025-10-30 08:19:25 +0800

    Added Icon to Login screen.

--- Payroller/Login.Designer.cs
+++ Payroller/Login.Designer.cs
@@ -17,16 +17,19 @@
 
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogIn));
             txtPassword = new TextBox();
             txtUsername = new TextBox();
             lblUsername = new Label();
             lblPassword = new Label();
             btnLogin = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // txtPassword
             // 
-            txtPassword.Location = new Point(129, 133);
+            txtPassword.Location = new Point(129, 156);
             txtPassword.Name = "txtPassword";
             txtPassword.Size = new Size(261, 27);
             txtPassword.TabIndex = 0;
@@ -34,7 +37,7 @@
             // 
             // txtUsername
             // 
-            txtUsername.Location = new Point(129, 100);
+            txtUsername.Location = new Point(129, 123);
             txtUsername.Name = "txtUsername";
             txtUsername.Size = new Size(261, 27);
             txtUsername.TabIndex = 1;
@@ -43,7 +46,7 @@
             // lblUsername
             // 
             lblUsername.AutoSize = true;
-            lblUsername.Location = new Point(12, 103);
+            lblUsername.Location = new Point(12, 126);
             lblUsername.Name = "lblUsername";
             lblUsername.Size = new Size(114, 20);
             lblUsername.TabIndex = 2;
@@ -52,7 +55,7 @@
             // lblPassword
             // 
             lblPassword.AutoSize = true;
-            lblPassword.Location = new Point(9, 136);
+            lblPassword.Location = new Point(9, 159);
             lblPassword.Name = "lblPassword";
             lblPassword.Size = new Size(113, 20);
             lblPassword.TabIndex = 3;
@@ -60,7 +63,7 @@
             // 
             // btnLogin
             // 
-            btnLogin.Location = new Point(197, 199);
+            btnLogin.Location = new Point(197, 222);
             btnLogin.Name = "btnLogin";
             btnLogin.Size = new Size(97, 30);
             btnLogin.TabIndex = 4;
@@ -68,11 +71,22 @@
             btnLogin.UseVisualStyleBackColor = true;
             btnLogin.Click += btnLogin_Click;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(197, 43);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(46, 42);
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
             // LogIn
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(427, 266);
+            Controls.Add(pictureBox1);
             Controls.Add(btnLogin);
             Controls.Add(lblPassword);
             Controls.Add(lblUsername);
@@ -82,6 +96,7 @@
             Text = "Login Sccreen";
             TopMost = true;
             Load += Passwordscreen_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -93,5 +108,6 @@
         private Label lblUsername;
         private Label lblPassword;
         private Button btnLogin;
+        private PictureBox pictureBox1;
     }
 }
